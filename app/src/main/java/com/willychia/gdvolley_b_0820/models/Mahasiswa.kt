18e3f24476e328e66eb03bbf151b20146f57d17d
CroZie19 <112044610+CroZie19@users.noreply.github.com>
2022-11-17 21:39:22 +0700

    Initial commit

---
+++ app/src/main/java/com/willychia/gdvolley_b_0820/models/Mahasiswa.kt
@@ -0,0 +1,8 @@
+package com.willychia.gdvolley_b_0820.models
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mahasiswa (var nama: String, var npm: String, var fakultas: String, var prodi: String) {
+    var id: Long? = null
+}
